39041ac2a23694b1c669754129c630024d197272
Muhammad Umer <127780703+1024umer@users.noreply.github.com>
2024-04-03 14:44:49 -0600

    signup frontend

--- resources/js/pages/SignUp.jsx
+++ resources/js/pages/SignUp.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+
 const SignUp = () => {
     const { subscriptionId } = useParams();
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        country: '',
+        streetAddress: '',
+        city: '',
+        region: '',
+        postalCode: '',
+        password: '',
+        password_confirmation: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            console.log(formData)
+            const response = await axios.post(`${import.meta.env.APP_URL}/signup`, formData);
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                Object.values(errorData.errors).forEach(error => {
+                    toast.error(error, {
+                        position: 'top-center'
+                    });
+                });
+            } else {
+                setFormData({
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    phone: '',
+                    country: '',
+                    streetAddress: '',
+                    city: '',
+                    region: '',
+                    postalCode: '',
+                    password: '',
+                    password_confirmation: ''
+                });
+                // Show success toast
+                toast.success('Signup successful!', {
+                    position: 'top-center'
+                });
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            toast.error('An error occurred. Please try again later.', {
+                position: 'top-center',
+                autoClose: 2000
+            });
+        }
+    };
+
     return (
         <>
             <section className='container mx-auto flex justify-center items-center h-screen'>
@@ -9,72 +73,86 @@ const SignUp = () => {
                     <h2 className='font-bold text-3xl text-violet-700'>Signup Page</h2>
                     <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
                     <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
-
-                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div className="sm:col-span-3">
-                            <label for="first-name" className="block text-sm font-medium leading-6 text-gray-900">First name</label>
-                            <div className="mt-2">
-                                <input type="text" name="first-name" id="first-name" autocomplete="given-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <form onSubmit={handleSubmit}>
+                        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                            <div className="sm:col-span-3">
+                                <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">First name</label>
+                                <div className="mt-2">
+                                    <input type="text" name="firstName" id="first-name" value={formData.firstName} onChange={handleChange} autoComplete="given-name" className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
+                            </div>
+                            <div className="sm:col-span-3">
+                                <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+                                <div className="mt-2">
+                                    <input type="text" name="lastName" id="last-name" value={formData.lastName} onChange={handleChange} autoComplete="family-name" className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
+                            </div>
+                            <div className="sm:col-span-3">
+                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                                <div className="mt-2">
+                                    <input type="email" name="email" id="email" value={formData.email} onChange={handleChange} autoComplete="email" className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
+                            </div>
+                            <div className="sm:col-span-3">
+                                <label htmlFor="phone" className="block text-sm font-medium leading-6 text-gray-900">Phone</label>
+                                <div className="mt-2">
+                                    <input type="number" name="phone" id="phone" value={formData.phone} onChange={handleChange} autoComplete="phone" className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
+                            </div>
+                            <div className="sm:col-span-3">
+                                <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">Country</label>
+                                <div className="mt-2">
+                                    <select name="country" id="country" value={formData.country} onChange={handleChange} autoComplete="country-name" className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                                        <option>United States</option>
+                                        <option>Canada</option>
+                                        <option>Mexico</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="col-span-full">
+                                <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">Street address</label>
+                                <div className="mt-2">
+                                    <input type="text" name="streetAddress" id="street-address" value={formData.streetAddress} onChange={handleChange} autoComplete="street-address" className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
+                            </div>
+                            <div className="sm:col-span-2 sm:col-start-1">
+                                <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">City</label>
+                                <div className="mt-2">
+                                    <input type="text" name="city" id="city" value={formData.city} onChange={handleChange} autoComplete="address-level2" className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
+                            </div>
+                            <div className="sm:col-span-2">
+                                <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">State / Province</label>
+                                <div className="mt-2">
+                                    <input type="text" name="region" id="region" value={formData.region} onChange={handleChange} autoComplete="address-level1" className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
+                            </div>
+                            <div className="sm:col-span-2">
+                                <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">ZIP / Postal code</label>
+                                <div className="mt-2">
+                                    <input type="text" name="postalCode" id="postal-code" value={formData.postalCode} onChange={handleChange} autoComplete="postal-code" className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
+                            </div>
+                            <div className="sm:col-span-4">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                                <div className="mt-2">
+                                    <input type="password" name="password" id="password" value={formData.password} onChange={handleChange} autoComplete="new-password" className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
+                            </div>
+                            <div className="sm:col-span-4">
+                                <label htmlFor="password_confirmation" className="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
+                                <div className="mt-2">
+                                    <input type="password" name="password_confirmation" id="password_confirmation" value={formData.password_confirmation} onChange={handleChange} autoComplete="new-password" className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
                             </div>
                         </div>
-
-                        <div className="sm:col-span-3">
-                            <label for="last-name" className="block text-sm font-medium leading-6 text-gray-900">Last name</label>
-                            <div className="mt-2">
-                                <input type="text" name="last-name" id="last-name" autocomplete="family-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                            </div>
-                        </div>
-
-                        <div className="sm:col-span-4">
-                            <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                            <div className="mt-2">
-                                <input id="email" name="email" type="email" autocomplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                            </div>
-                        </div>
-
-                        <div className="sm:col-span-3">
-                            <label for="country" className="block text-sm font-medium leading-6 text-gray-900">Country</label>
-                            <div className="mt-2">
-                                <select id="country" name="country" autocomplete="country-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                                    <option>United States</option>
-                                    <option>Canada</option>
-                                    <option>Mexico</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div className="col-span-full">
-                            <label for="street-address" className="block text-sm font-medium leading-6 text-gray-900">Street address</label>
-                            <div className="mt-2">
-                                <input type="text" name="street-address" id="street-address" autocomplete="street-address" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                            </div>
-                        </div>
-
-                        <div className="sm:col-span-2 sm:col-start-1">
-                            <label for="city" className="block text-sm font-medium leading-6 text-gray-900">City</label>
-                            <div className="mt-2">
-                                <input type="text" name="city" id="city" autocomplete="address-level2" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                            </div>
-                        </div>
-
-                        <div className="sm:col-span-2">
-                            <label for="region" className="block text-sm font-medium leading-6 text-gray-900">State / Province</label>
-                            <div className="mt-2">
-                                <input type="text" name="region" id="region" autocomplete="address-level1" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                            </div>
-                        </div>
-
-                        <div className="sm:col-span-2">
-                            <label for="postal-code" className="block text-sm font-medium leading-6 text-gray-900">ZIP / Postal code</label>
-                            <div className="mt-2">
-                                <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                            </div>
-                        </div>
-                    </div>
+                        <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Submit</button>
+                    </form>
                 </div>
             </section>
+            <ToastContainer />
         </>
-    )
+    );
 }
 
-export default SignUp
+export default SignUp;
