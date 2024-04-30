0f566ed6dd2f343ad72b01c6cd60479b66572222
Rao Talha <talharao997az@gmail.com>
2024-04-30 05:42:29 +0500

    bidCommerce data get

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,21 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\OrderResource;
 use App\Models\User;
-use App\Services\Clover\OrderCloverUserService;
 use Illuminate\Http\Request;
+use App\Http\Resources\OrderResource;
+use App\Services\Clover\OrderCloverUserService;
+use App\Services\BigCommerce\OrderBigCommerceService;
 
 class OrderController extends Controller
 {
-    public function getOrders($id,OrderCloverUserService $orderClover){
+    public function getOrders($id, OrderBigCommerceService $bigCommerce, OrderCloverUserService $orderClover)
+    {
         $user = User::find($id);
-        if($user){
-            $data  = $orderClover->getAllOrders($user);
-            // dd($data);
-            return new OrderResource($data);
-        }else{
+        if ($user) {
+            $data = $orderClover->getAllOrders($user);
+            $data2 = $bigCommerce->bigCommerceOrders($user);
+            return new OrderResource($data2);
+        } else {
             return response()->json(['message' => 'User not found'], 404);
         }
     }
+
 }
---
+++ app/Services/BigCommerce/OrderBigCommerceService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services\BigCommerce;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Http;
+
+class OrderBigCommerceService
+{
+
+public function bigCommerceOrders($user){
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://api.bigcommerce.com/stores/".env('BIGCOMMERCE_STORE_HASH')."/v2/orders",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => [
+    "Accept: application/json",
+    "Content-Type: application/json",
+    "X-Auth-Token: ".env('BIGCOMMERCE_AUTH_TOKEN')
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    $orders = json_decode($response, true);
+    $customerOrders = [];
+
+    foreach ($orders as $order) {
+        if ($user->bigcommerce_id === $order['customer_id']) {
+            $customerOrders[] = $order;
+        }
+    }
+    return $customerOrders;
+}
+
+}
+}
