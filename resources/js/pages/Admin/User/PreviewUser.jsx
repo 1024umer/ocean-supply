8b9c80ddc3f94c2593f35339c3f3b35dcccab1bb
Muhammad Umer <127780703+1024umer@users.noreply.github.com>
2024-05-06 14:01:00 -0600

    tables

--- resources/js/pages/Admin/User/PreviewUser.jsx
+++ resources/js/pages/Admin/User/PreviewUser.jsx
@@ -24,82 +24,84 @@ export const PreviewUser = () => {
     return (
         <>
             <SidebarMain />
-            <div className="overflow-x-auto w-full lg:p-5 mt-5 ">
-                <h2 className="text-xl font-bold my-3">BigCommerce Orders</h2>
-                <Table hoverable>
-                    <Table.Head>
-                        {/* <Table.HeadCell>#</Table.HeadCell> */}
-                        <Table.HeadCell>Product name</Table.HeadCell>
-                        <Table.HeadCell>Price</Table.HeadCell>
-                        <Table.HeadCell>Date</Table.HeadCell>
-                        <Table.HeadCell>Details</Table.HeadCell>
-                    </Table.Head>
-                    <Table.Body className="divide-y">
-                        {bigCommerceOrders.map(order => {
-                            // console.log(order);
-                            return (
-                                <Table.Row key={order.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                                    {/* <Tabel.Cell>{order.id}</Tabel.Cell> */}
-                                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                                        {order.lineItems ? order.lineItems.elements[0].name : ''}
-                                    </Table.Cell>
-                                    <Table.Cell>${order.total_inc_tax}</Table.Cell>
-                                    <Table.Cell>{new Date(order.date_created).toLocaleDateString()}</Table.Cell>
-                                    <Table.Cell>
-                                        <Link
-                                            to={'/order/'+order.id}
-                                            className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-                                        >
-                                            View Details
-                                        </Link>
+            <div className="grid grid-cols-12 w-full">
+                <div className="overflow-x-auto w-full lg:p-5 mt-5 col-span-12">
+                    <h2 className="text-xl font-bold my-3">BigCommerce Orders</h2>
+                    <Table hoverable>
+                        <Table.Head>
+                            {/* <Table.HeadCell>#</Table.HeadCell> */}
+                            <Table.HeadCell>Product name</Table.HeadCell>
+                            <Table.HeadCell>Price</Table.HeadCell>
+                            <Table.HeadCell>Date</Table.HeadCell>
+                            <Table.HeadCell>Details</Table.HeadCell>
+                        </Table.Head>
+                        <Table.Body className="divide-y">
+                            {bigCommerceOrders.map(order => {
+                                // console.log(order);
+                                return (
+                                    <Table.Row key={order.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                        {/* <Tabel.Cell>{order.id}</Tabel.Cell> */}
+                                        <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                            {order.lineItems ? order.lineItems.elements[0].name : ''}
+                                        </Table.Cell>
+                                        <Table.Cell>${order.total_inc_tax}</Table.Cell>
+                                        <Table.Cell>{new Date(order.date_created).toLocaleDateString()}</Table.Cell>
+                                        <Table.Cell>
+                                            <Link
+                                                to={'/order/'+order.id}
+                                                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                                            >
+                                                View Details
+                                            </Link>
 
-                                    </Table.Cell>
-                                </Table.Row>
-                            )
+                                        </Table.Cell>
+                                    </Table.Row>
+                                )
 
-                        })}
+                            })}
 
-                    </Table.Body>
-                </Table>
-            </div>
+                        </Table.Body>
+                    </Table>
+                </div>
 
-            <div className="overflow-x-auto w-full lg:p-5 mt-5">
-                <h2 className="text-xl font-bold my-3">Clover Orders</h2>
-                <Table hoverable>
-                    <Table.Head>
-                        {/* <Table.HeadCell>#</Table.HeadCell> */}
-                        <Table.HeadCell>Product name</Table.HeadCell>
-                        <Table.HeadCell>Price</Table.HeadCell>
-                        <Table.HeadCell>Date</Table.HeadCell>
-                        <Table.HeadCell>Details</Table.HeadCell>
-                    </Table.Head>
-                    <Table.Body className="divide-y">
-                        {cloverOrders.map(order => {
-                            // console.log(order);
-                            return (
-                                <Table.Row key={order.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                                    {/* <Tabel.Cell>{order.id}</Tabel.Cell> */}
-                                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                                        {order.lineItems ? order.lineItems.elements[0].name : ''}
-                                    </Table.Cell>
-                                    <Table.Cell>${order.lineItems ? order.lineItems.elements[0].price : ''}</Table.Cell>
-                                    <Table.Cell>{new Date(order.createdTime).toLocaleDateString()}</Table.Cell>
-                                    <Table.Cell>
-                                        <Link
-                                            to={'/order/'+order.id}
-                                            className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-                                        >
-                                            View Details
-                                        </Link>
+                <div className="overflow-x-auto w-full lg:p-5 mt-5 col-span-12">
+                    <h2 className="text-xl font-bold my-3">Clover Orders</h2>
+                    <Table hoverable>
+                        <Table.Head>
+                            {/* <Table.HeadCell>#</Table.HeadCell> */}
+                            <Table.HeadCell>Product name</Table.HeadCell>
+                            <Table.HeadCell>Price</Table.HeadCell>
+                            <Table.HeadCell>Date</Table.HeadCell>
+                            <Table.HeadCell>Details</Table.HeadCell>
+                        </Table.Head>
+                        <Table.Body className="divide-y">
+                            {cloverOrders.map(order => {
+                                // console.log(order);
+                                return (
+                                    <Table.Row key={order.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                        {/* <Tabel.Cell>{order.id}</Tabel.Cell> */}
+                                        <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                            {order.lineItems ? order.lineItems.elements[0].name : ''}
+                                        </Table.Cell>
+                                        <Table.Cell>${order.lineItems ? order.lineItems.elements[0].price : ''}</Table.Cell>
+                                        <Table.Cell>{new Date(order.createdTime).toLocaleDateString()}</Table.Cell>
+                                        <Table.Cell>
+                                            <Link
+                                                to={'/order/'+order.id}
+                                                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                                            >
+                                                View Details
+                                            </Link>
 
-                                    </Table.Cell>
-                                </Table.Row>
-                            )
+                                        </Table.Cell>
+                                    </Table.Row>
+                                )
 
-                        })}
+                            })}
 
-                    </Table.Body>
-                </Table>
+                        </Table.Body>
+                    </Table>
+                </div>
             </div>
         </>
     );
