668618c878a797526d01540eaa164126fe4d6cd1
Muhammad Umer <127780703+1024umer@users.noreply.github.com>
2024-05-08 15:08:14 -0600

    listing

--- resources/js/pages/Admin/Inventory/InventoryList.jsx
+++ resources/js/pages/Admin/Inventory/InventoryList.jsx
@@ -4,27 +4,33 @@ import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import SidebarMain from "../../../components/SidebarMain";
 import Loading from "../../../components/Loading";
+import Box from "../../../components/Box";
 function InventoryList() {
     const [inventory, setInventory] = useState([]);
     const [loading, setLoading] = useState(true);
     const getInventories = async () => {
-        const response = await service.get("/api/inventory");
-        setInventory(response.data.data);
-        setLoading(false)
+        const response = await service.get("/api/inventory").then(response => {
+            setInventory(response.data.elements);
+            setLoading(false)
+        }).catch(setLoading(true));
     };
 
     useEffect(() => {
         getInventories();
     }, []);
     if (loading) {
-        return <Loading/> ;
+        return <Loading />;
     }
-  return (
-    <>
-        <SidebarMain />
-        
-    </>
-  )
+    return (
+        <>
+            <SidebarMain />
+            <div className="grid grid-cols-7 gap-6 p-4 mt-4">
+                {inventory.map(item => (
+                    <Box key={item.id} title={item.name} total={item.price} />
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default InventoryList
